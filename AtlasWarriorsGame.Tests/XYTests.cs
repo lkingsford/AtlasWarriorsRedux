4dc82b594b90d738e8321bf1e9d1a69763bf2bca
Lachlan Kingsford <lachlan.kingsford@cornerstonesolutions.com.au>
2017-06-10 23:01:50 +1000

    Added XY + - == operators

--- AtlasWarriorsGame.Tests/XYTests.cs
+++ AtlasWarriorsGame.Tests/XYTests.cs
@@ -48,5 +48,83 @@ namespace AtlasWarriorsGame.Tests
         {
             Assert.AreEqual(new XY(X, Y).ToString(), ToStringResult);
         }
+
+        /// <summary>
+        /// Points for testing subtraction - operand 1, operand 2, correct result
+        /// </summary>
+        static object[] SubValues =
+        {
+            new object[] {new XY(0,0), new XY(0,0), new XY(0,0) },
+            new object[] {new XY(1,0), new XY(0,0), new XY(1,0) },
+            new object[] {new XY(5,5), new XY(2,2), new XY(3,3) },
+            new object[] {new XY(1,5), new XY(1,2), new XY(0,3) },
+            new object[] {new XY(0,0), new XY(-1,0), new XY(1,0) },
+            new object[] {new XY(-1,-1), new XY(-1,0), new XY(0,-1) },
+            new object[] {new XY(1,1), new XY(3,0), new XY(-2,1) }
+        };
+
+        /// <summary>
+        /// Test addition
+        /// </summary>
+        /// <param name="Op1">First operand</param>
+        /// <param name="Op2">Second operand</param>
+        /// <param name="Result">Correct result</param>
+        [Test]
+        [TestCaseSource("SubValues")]
+        public void SubTest(XY Op1, XY Op2, XY Result)
+        {
+           // IsTrue, because AreEqual wasn't using custom ==
+           Assert.IsTrue((Op1 - Op2) == Result); 
+        }
+
+
+        /// <summary>
+        /// Points for testing addition - operand 1, operand 2, correct result
+        /// </summary>
+        static object[] AddValues =
+        {
+            new object[] {new XY(0,0), new XY(0,0), new XY(0,0) },
+            new object[] {new XY(1,0), new XY(0,0), new XY(1,0) },
+            new object[] {new XY(5,5), new XY(2,2), new XY(7,7) },
+            new object[] {new XY(1,5), new XY(1,2), new XY(2,7) },
+            new object[] {new XY(0,0), new XY(-1,0), new XY(-1,0) },
+            new object[] {new XY(-1,-1), new XY(-1,0), new XY(-2,-1) },
+        };
+
+        /// <summary>
+        /// Test addition
+        /// </summary>
+        /// <param name="Op1">First operand</param>
+        /// <param name="Op2">Second operand</param>
+        /// <param name="Result">Correct result</param>
+        [Test]
+        [TestCaseSource("AddValues")]
+        public void AddTest(XY Op1, XY Op2, XY Result)
+        {
+           // IsTrue, because AreEqual wasn't using custom ==
+           Assert.IsTrue((Op1 + Op2) == Result); 
+        }
+
+        /// <summary>
+        /// Points for testing equality
+        /// </summary>
+        static object[] EqualsValues =
+        {
+            new XY(0,0) ,
+            new XY(5,0),
+            new XY(-1,-1),
+            new XY(-1,0)
+        };
+
+        /// <summary>
+        /// Test that equals is correct
+        /// </summary>
+        /// <param name="Op1"></param>
+        [Test]
+        [TestCaseSource("EqualsValues")]
+        public void EqualsTest(XY Op1)
+        {
+            Assert.AreEqual(Op1, Op1);
+        }
     }
 }
--- AtlasWarriorsGame/XY.cs
+++ AtlasWarriorsGame/XY.cs
@@ -57,5 +57,60 @@ namespace AtlasWarriorsGame
         {
             return $"({X}, {Y})";
         }
+
+        /// <summary>
+        /// Addition operator for XY. Adds X to X and Y to Y - cartesian addition.
+        /// </summary>
+        /// <param name="first">First operand</param>
+        /// <param name="second">Second operand</param>
+        /// <returns></returns>
+        public static XY operator +(XY first, XY second)
+        {
+            return new XY(first.X + second.X, first.Y + second.Y);
+        }
+
+        /// <summary>
+        /// Subtraction operator for XY. Subtracts X from X and Y from Y.
+        /// </summary>
+        /// <param name="first">First operand</param>
+        /// <param name="second">Second operand</param>
+        /// <returns></returns>
+        public static XY operator -(XY first, XY second)
+        {
+            return new XY(first.X - second.X, first.Y - second.Y);
+        }
+
+        /// <summary>
+        /// Equality operator for XY
+        /// </summary>
+        /// <param name="first">First operand</param>
+        /// <param name="second">Second operand</param>
+        /// <returns></returns>
+        public static bool operator ==(XY first, XY second)
+        {
+            return (first.X == second.X) && (first.Y == second.Y);
+        }
+
+        /// <summary>
+        /// Equality operator for XY
+        /// </summary>
+        /// <param name="first">First operand</param>
+        /// <param name="second">Second operand</param>
+        /// <returns></returns>
+        public static bool Equals(XY first, XY second)
+        {
+            return first == second;
+        }
+        /// <summary>
+        /// Inequality operator for XY
+        /// </summary>
+        /// <param name="first">First operand</param>
+        /// <param name="second">Second operand</param>
+        /// <returns></returns>
+        public static bool operator !=(XY first, XY second)
+        {
+            return !(first == second);
+        }
+
     }
 }
