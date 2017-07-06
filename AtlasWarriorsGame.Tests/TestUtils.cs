e97bf70ed41bbbcab437d8f8504bba1d9156d76b
Lachlan Kingsford <lachlan@nerdygentleman.com>
2017-07-06 23:04:08 +1000

    #12 Moved GetFeature and GetDungeon to TestUtils

---
+++ AtlasWarriorsGame.Tests/TestUtils.cs
@@ -0,0 +1,92 @@
+﻿using AtlasWarriorsGame.DungeonGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AtlasWarriorsGame.Dungeon;
+
+namespace AtlasWarriorsGame.Tests
+{
+    /// <summary>
+    /// Tools for assisting with writing tests
+    /// </summary>
+    static class TestUtils
+    {
+        /// <summary>
+        /// Convert string of " ", "#", "+", "-" and "." to a dungeon
+        /// for testing
+        /// </summary>
+        /// <param name="MapToSet">Array of " " and "#"</param>
+        /// <returns>Filling for given map</returns>
+        public static AtlasWarriorsGame.Dungeon GetDungeon(params string[] MapToSet)
+        {
+            var width = MapToSet[0].Length;
+            var height = MapToSet.Length;
+            var d = new Dungeon(width, height);
+
+            for (var ix = 0; ix < width; ++ix)
+            {
+                for (var iy = 0; iy < height; ++iy)
+                {
+                    switch (MapToSet[iy][ix])
+                    {
+                        case ' ':
+                            d.SetCell(new XY(ix, iy), DungeonCell.EMPTY);
+                            break;
+                        case '+':
+                            d.SetCell(new XY(ix, iy), DungeonCell.DOOR);
+                            break;
+                        case '#':
+                            d.SetCell(new XY(ix, iy), DungeonCell.WALL);
+                            break;
+                        case '.':
+                            d.SetCell(new XY(ix, iy), DungeonCell.FLOOR);
+                            break;
+                    }
+                }
+            }
+
+            return d;
+        }
+
+
+        /// <summary>
+        /// Convert string of " ", "#", "+", "-" and "." to a feature, adding doors on '+'s
+        /// for testing
+        /// </summary>
+        /// <param name="MapToSet">Array of " " and "#"</param>
+        /// <returns>Filling for given map</returns>
+        public static Feature GetFeature(params string[] MapToSet)
+        {
+            var width = MapToSet[0].Length;
+            var height = MapToSet.Length;
+            var f = new Feature(width, height);
+
+            for (var ix = 0; ix < width; ++ix)
+            {
+                for (var iy = 0; iy < height; ++iy)
+                {
+                    switch (MapToSet[iy][ix])
+                    {
+                        case ' ':
+                            f.SetCell(new XY(ix, iy), DungeonCell.EMPTY);
+                            break;
+                        case '+':
+                            f.SetCell(new XY(ix, iy), DungeonCell.DOOR);
+                            f.AddPossibleDoor(new XY(ix, iy));
+                            break;
+                        case '#':
+                            f.SetCell(new XY(ix, iy), DungeonCell.WALL);
+                            break;
+                        case '.':
+                            f.SetCell(new XY(ix, iy), DungeonCell.FLOOR);
+                            break;
+                    }
+                }
+            }
+
+            return f;
+        }
+    }
+}
