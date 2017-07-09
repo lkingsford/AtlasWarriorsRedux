50d8737ea1f449b94deef2c6ddfa980928b5283e
Lachlan Kingsford <lachlan@nerdygentleman.com>
2017-07-09 23:30:51 +1000

    76 Added random from list

---
+++ AtlasWarriorsGame.Tests/GlobalRandom.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AtlasWarriorsGame.GlobalRandom;
+
+namespace AtlasWarriorsGame.Tests
+{
+    /// <summary>
+    /// Tests for the GlobalRandom class
+    /// </summary>
+    [TestFixture]
+    class GlobalRandom
+    {
+        /// <summary>
+        /// Test that R.RandomItem returns an element from its list
+        /// </summary>
+        [Test]
+        public void TestListRandom()
+        {
+            var list = new List<int> { 0, 1, 2, 3, 4, 5, 6 };
+            var random = list.RandomItem();
+            Assert.Contains(random, list, "List item not found");
+        }
+    }
+}
--- AtlasWarriorsGame/GlobalRandom.cs
+++ AtlasWarriorsGame/GlobalRandom.cs
@@ -9,6 +9,21 @@ namespace AtlasWarriorsGame
     /// </summary>
     public static class GlobalRandom
     {
+        /// <summary>
+        /// Random class, initialised once
+        /// </summary>
         public static System.Random R = new System.Random();
+
+        /// <summary>
+        /// Extension method that returns a random element of a list using the Global Random
+        /// generator
+        /// </summary>
+        /// <typeparam name="T">Type in list</typeparam>
+        /// <param name="source">List to select from</param>
+        /// <returns>Random T from list</returns>
+        public static T RandomItem<T>(this IList<T> source)
+        {
+            return source[R.Next(source.Count)];
+        }
     }
 }
