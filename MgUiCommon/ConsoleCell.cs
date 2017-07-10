d3c4011fe9ca6fb80c277d509147603c5d61f509
Lachlan Kingsford <lachlan@nerdygentleman.com>
2017-07-10 21:04:50 +1000

    79 Moved ConsoleCell

---
+++ MgUiCommon/ConsoleCell.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MgUiCommon
+{
+    /// <summary>
+    /// Cell in the console display
+    /// </summary>
+    public struct ConsoleCell
+    {
+        /// <summary>
+        /// Character that will be drawn
+        /// </summary>
+        public char DrawChar;
+
+        /// <summary>
+        /// Color in the background of the cell
+        /// </summary>
+        public Color BackColor;
+
+        /// <summary>
+        /// Color in the foreground of the cell
+        /// </summary>
+        public Color ForeColor;
+    }
+
+}
--- MgUiCommon/MapViewElement.cs
+++ MgUiCommon/MapViewElement.cs
@@ -91,27 +91,6 @@ namespace MgUiCommon
         private Dungeon LastTurnDungeon;
 
         /// <summary>
-        /// Cell in the console display
-        /// </summary>
-        public struct ConsoleCell
-        {
-            /// <summary>
-            /// Character that will be drawn
-            /// </summary>
-            public char DrawChar;
-
-            /// <summary>
-            /// Color in the background of the cell
-            /// </summary>
-            public Color BackColor;
-
-            /// <summary>
-            /// Color in the foreground of the cell
-            /// </summary>
-            public Color ForeColor;
-        }
-
-        /// <summary>
         /// Text console that will be drawn
         /// </summary>
         public ConsoleCell[,] ConsoleBuffer;
