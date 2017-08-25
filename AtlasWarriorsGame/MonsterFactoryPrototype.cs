815b400e236db0570fd63f2df84dacaa360acfec
Lachlan Kingsford <lachlan@nerdygentleman.com>
2017-08-25 23:10:22 +1000

    5 49 22 Added monsters from factory

---
+++ AtlasWarriorsGame/MonsterFactoryPrototype.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtlasWarriorsGame
+{
+    /// <summary>
+    /// Data that is stored from a JSON file to create a monster with
+    /// </summary>
+    class MonsterFactoryPrototype
+    {
+        public string SpriteId = "";
+        public string Description = "";
+        public int Health = 10;
+        public int BaseAtk = 10;
+        public int BaseDef = 10;
+        public int BaseDmg = 10;
+    }
+}
